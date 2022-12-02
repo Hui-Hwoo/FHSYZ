c253e0b96353c018ae67dabbd6dcfe2878d7339e
Hui Hwoo <hui.hwoo@gmail.com>
2022-12-02 15:31:06 -0800

    init

---
+++ run.sh
@@ -0,0 +1,18 @@
+# Setup the python virtual environment
+python3.9 -m venv venv
+source venv/bin/activate
+
+# Install all necessary library
+pip install --upgrade pip
+pip install  -r requirements.txt
+
+# Setup the development environment
+export FLASK_APP=app.py   
+export FLASK_ENV=development
+
+# Activate debug mode
+export FLASK_DEBUG=1
+
+# start the program
+flask run
+
